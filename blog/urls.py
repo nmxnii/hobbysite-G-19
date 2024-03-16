eda9e56cd8e2f2721bdaa7b230d0d78d2fd9b2a2
imamamo <malena.delrosario@student.ateneo.edu>
2024-03-16 21:18:56 +0800

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import BlogArticleView, BlogDetailView
 
 urlpatterns = [
-    path('blogs/article/', BlogArticleView.as_view(), name='article'),
-    path('blog/article/1', BlogDetailView.as_view(), name=
+    path('blog/articles/', BlogArticleView.as_view(), name='article'),
+    path('blog/article/<int:pk>', BlogDetailView.as_view(), name=
          'blog_detail')
 ]
