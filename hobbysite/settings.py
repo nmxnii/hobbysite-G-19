74e7add2551587075037fb388db9a0eb0a211390
nmxnii <nicole.manibog@student.ateneo.edu>
2024-03-20 23:14:52 +0800

    Fixed urls and settings

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'merchstore'
-    'wiki'
-    'blog'
-    'commissions'
+    'merchstore',
+    'wiki',
+    'blog',
+    'commissions',
 ]
 
 MIDDLEWARE = [
--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('merchstore/', include('merchstore.urls', namespace="merchstore")),
     path('wiki/', include("wiki.urls", namespace="wiki")),
-    path('blog/', include('blog.urls', namespace ='blog'))
-    path('commissions/', include("commissions.urls", namespace="commissions"))
+    path('blog/', include('blog.urls', namespace ='blog')),
+    path('commissions/', include("commissions.urls", namespace="commissions")),
 
 ]
 
