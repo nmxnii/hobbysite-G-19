07809f205255718ee368a91c74900332543d4819
Gershwin <gershwin.hierro@student.ateneo.edu>
2024-03-20 22:40:52 +0800

    edited settings and urls.py

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
-
-
+    path('merchstore/', include('merchstore.urls', namespace="merchstore")),
 
     path('commissions/', include("commissions.urls", namespace="commissions"))
 
