61f78a1e9a697f74962dc9afbae5e80c36f4f1bd
Kyleisha Umbay <marie.umbay@student.ateneo.edu>
2024-03-20 22:43:28 +0800

    fixed merge issues2

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    
-    path('wiki/', include("wiki.urls", namespace="wiki"))
+    path('merchstore/', include('merchstore.urls', namespace="merchstore")),
+    path('wiki/', include("wiki.urls", namespace="wiki")),
 
 
 ]
