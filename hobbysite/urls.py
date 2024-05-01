63e415cee47f141e9f72f8dc0b75aa2a82ee94e3
Gershwin Hierro <gershwinchierro@gmail.com>
2024-05-01 17:26:18 +0800

    included more paths for accounts/

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("blog/", include("blog.urls", namespace="blog")),
     path("commissions/", include("commissions.urls", namespace="commissions")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),    
     path("profile/", include("user_management.urls", namespace="profile")),
     path("home/", home),
 ]
