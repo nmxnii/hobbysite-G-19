e51a09994dee8a36b366f585873cffd1f1910906
Gershwin <gershwin.hierro@student.ateneo.edu>
2024-05-03 19:49:36 +0800

    added more functionality to the user_management app

--- user_management/urls.py
+++ user_management/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
     path('<int:pk>/update', ProfileUpdateView.as_view(), name='profile-update'),
+    path('<int:pk>/create', ProfileCreateView.as_view(), name='profile-create'),
 ]
 
 app_name = 'user_management'
--- user_management/views.py
+++ user_management/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Profile
@@ -7,18 +7,24 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
-class ProfileUpdateView(LoginRequiredMixin,UpdateView):
+class ProfileUpdateView(UpdateView):
     model = Profile
     form_class= ProfileUpdateForm
     template_name= 'profile_update.html'
     
     def get_success_url(self) -> str:
-        return reverse_lazy("profile:profile-detail", kwargs={"pk": self.object.pk})
+        return reverse_lazy("profile:profile-detail", kwargs={"pk": self.object.user.pk})
 
-class ProfileDetailView(LoginRequiredMixin,DetailView):
+class ProfileDetailView(DetailView):
     model=Profile
     template_name='profile_detail.html'
-    
+
+class ProfileCreateView(CreateView):
+    model=Profile  
+    form_class=ProfileCreateForm
+    template_name= 'profile_update.html'
+    def get_success_url(self) -> str:
+        return reverse_lazy("profile:profile-detail", kwargs={"pk": self.object.user.pk})
     
 
 # Create your views here.
