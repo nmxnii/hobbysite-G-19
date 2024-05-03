3feb210c4b3bbfb71b0a746dedcc2484e2814480
Gershwin <gershwin.hierro@student.ateneo.edu>
2024-05-03 22:30:54 +0800

    edited commissions/models.py

--- commissions/models.py
+++ commissions/models.py
@@ -8,6 +8,9 @@ class Commission(models.Model):
     people_required = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=(('open', 'OPEN'), ('full', 'FULL'),
+                                       ('completed', 'COMPLETED'), ('discontinued', 'DISCONTINUED'))
+                                       , default='open', max_length=255)
 
     class Meta:
         ordering = ['created_on']
@@ -19,17 +22,31 @@ class Commission(models.Model):
         return reverse("commissions:commissions-detail", args=str(self.pk))
 
 
-class Comment(models.Model):
+class Job(models.Model):
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
-    entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return 'Comment'
+    role = models.CharField(max_length=255)
+    manpower_required = models.IntegerField()
+    status = models.CharField(
+        choices=(('open', 'OPEN'), ('full', 'FULL')), default='open', max_length=255)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = [['open', 'full'], '-manpower_required', 'role']
+    # entry=models.TextField()
+    # created_on=models.DateTimeField(auto_now_add=True)
+    # updated_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Job'
+
+
+class JobApplication(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    status = models.CharField(choices=(
+        ('pending', 'PENDING'), ('accepted', 'ACCEPTED'), ('rejected', 'REJECTED')), default='pending', max_length=255)
+    applied_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = [['pending', 'accepted', 'rejected'], '-applied_on']
 
 
 # Create your models here.
