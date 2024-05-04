d185e63a12d9066b75c05f5fe13f4226708c28ae
imamamo <malena.delrosario@student.ateneo.edu>
2024-05-04 13:19:03 +0800

    added feature to add and update article

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.db.models import Max
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
+from django.urls import reverse_lazy
+
+
+from .forms import ArticleForm, ArticleUpdateForm
 from .models import Article, ArticleCategory
 
 
@@ -19,3 +24,28 @@ class BlogArticleView(ListView):
 class BlogDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = "article_create.html"
+
+    def get_success_url(self):
+        return reverse_lazy("blog:article_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleUpdateForm
+    template_name = "article_update.html"
+
+    def get_success_url(self):
+        return reverse_lazy("blog:article_detail", kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+    
