eaff13b0b420f27246a6e557865cdedb44574412
imamamo <malena.delrosario@student.ateneo.edu>
2024-05-04 11:09:55 +0800

    added feature to categorize articles by category

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.db.models import Max
 
-from .models import Article
+from .models import Article, ArticleCategory
 
 
 class BlogArticleView(ListView):
     model = Article
     template_name = "article_list.html"
 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)        
+        context['article_category'] = ArticleCategory.objects.all()
+        
+        return context
+    
 class BlogDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
