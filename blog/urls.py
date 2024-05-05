a65c807e0df6fb9532b85f383d2068bb271fe7d2
imamamo <malena.delrosario@student.ateneo.edu>
2024-05-05 15:18:05 +0800

    added view for image gallery

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 
-from .views import BlogArticleView, BlogDetailView, ArticleCreateView, ArticleUpdateView
+from .views import BlogArticleView, BlogDetailView, ArticleCreateView, ArticleUpdateView, ImageGalleryView
 
 urlpatterns = [
     path('articles/', BlogArticleView.as_view(), name='article_list'),
     path('article/<int:pk>', BlogDetailView.as_view(), name='article_detail'),
     path('article/add', ArticleCreateView.as_view(), name='article_create'),
     path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name='article_update'),
-
+    path('gallery/articles', ImageGalleryView.as_view(), name='blog_gallery_view'),
 ]
 
 app_name = 'blog'
