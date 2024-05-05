2daa44c5c642b94748d1615e2ae292a29ca7c71a
imamamo <malena.delrosario@student.ateneo.edu>
2024-05-05 15:21:08 +0800

    added comment and image gallery feature

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
@@ -6,7 +7,7 @@ from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
 
 
-from .forms import ArticleForm, ArticleUpdateForm
+from .forms import ArticleForm, CommentForm
 from .models import Article, ArticleCategory
 
 
@@ -25,6 +26,25 @@ class BlogDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article = self.get_object()
+        context['comment_form'] = CommentForm() 
+        context['comments'] = article.comment_set.all()  # Get all comments for the article
+        return context
+
+    def post(self, request, *args, **kwargs):
+        article = self.get_object()
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.author = request.user.profile
+            new_comment.article = article
+            new_comment.save()
+            return redirect('blog:article_detail', pk=article.pk)
+        else:
+            return self.render_to_response(self.get_context_data(comment_form=comment_form))
+
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleForm
@@ -39,7 +59,7 @@ class ArticleCreateView(CreateView):
 
 class ArticleUpdateView(UpdateView):
     model = Article
-    form_class = ArticleUpdateForm
+    form_class = ArticleForm
     template_name = "article_update.html"
 
     def get_success_url(self):
@@ -48,4 +68,12 @@ class ArticleUpdateView(UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user.profile
         return super().form_valid(form)
-    
+
+class ImageGalleryView(ListView):
+    model = Article
+    template_name = "blog_gallery_view.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()  # Assuming you want to display all articles
+        return context
