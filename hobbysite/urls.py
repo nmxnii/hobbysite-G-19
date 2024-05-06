7c54c6bf8934e61c623fb9789c3ca4ccd08b7edb
Gershwin <gershwin.hierro@student.ateneo.edu>
2024-05-06 17:26:00 +0800

    added name to homepage

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),    
     path("profile/", include("user_management.urls", namespace="profile")),
-    path("home/", home),
+    path("home/", home, name='home'),
 ]
