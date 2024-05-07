7abde6da21afa94356f33cea03f1d3b6e8a2a4b6
Kyleisha Umbay <marie.umbay@student.ateneo.edu>
2024-05-07 22:07:39 +0800

    updated for media

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home
 
 urlpatterns = [
@@ -30,3 +32,5 @@ urlpatterns = [
     path("profile/", include("user_management.urls", namespace="profile")),
     path("home/", home, name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
