c888138da07b3635a928411ec54fb195a6b1fd43
Gershwin <gershwin.hierro@student.ateneo.edu>
2024-05-08 16:31:18 +0800

    fixed formatting and updated fields

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
-from .models import Commission, Job
+from .models import *
 
 
 class JobInline(admin.TabularInline):
     model = Job
 
+class JobApplicationInline(admin.TabularInline):
+    model=JobApplication
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
     list_display = ('title', 'description','created_on', 'updated_on')
     inlines = [JobInline,]
 
+class JobAdmin(admin.ModelAdmin):
+    model=Job
+    list_display=('status', 'manpower_required', 'role')
+    inlines=[JobApplicationInline,]
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    model=JobApplication
 
 admin.site.register(Commission, CommissionAdmin)
-
+admin.site.register(Job,JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
