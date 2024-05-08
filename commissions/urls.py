a4db543d7c1380cba0f4c96e0fcbc527e5c836d1
Gershwin <gershwin.hierro@student.ateneo.edu>
2024-05-08 16:33:15 +0800

    finalized urls and fixed formatting

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CommissionDetailView, CommissionListView
+from .views import *
 
 urlpatterns = [
     path('list/', CommissionListView.as_view(), name="commissions"),
-    path('detail/<int:pk>', CommissionDetailView.as_view(),
-         name='commissions-detail')
+    path('detail/<int:pk>', CommissionDetailView.as_view(), name='commissions-detail'),
+    path('add/', CommissionCreateView.as_view(), name='commissions-create'),
+    path('<int:pk>/edit/', CommissionUpdateView.as_view(), name='commissions-update'),
 ]
 
 app_name = 'commissions'
