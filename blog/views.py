40b82c39c773b54424c141e7dee3882ee17bf6c1
imamamo <malena.delrosario@student.ateneo.edu>
2024-05-10 16:59:38 +0800

    fixed bug on other articles by author

--- blog/views.py
+++ blog/views.py
@@ -28,13 +28,14 @@ class BlogDetailView(DetailView):
     template_name = "article_detail.html"
 
     def get_context_data(self, **kwargs):
-        articles_by_author = Article.objects.filter(author=self.object.author).exclude(pk=self.object.pk)[:2]
-        context['articles_by_author'] = articles_by_author
         context = super().get_context_data(**kwargs)
         article = self.get_object()
+
+        articles_by_author = Article.objects.filter(author=self.object.author).exclude(pk=self.object.pk)[:2]
+        context['articles_by_author'] = articles_by_author
         context['comment_form'] = CommentForm() 
         context['comments'] = article.comment_set.all() 
-        
+
         return context
 
     def post(self, request, *args, **kwargs):
