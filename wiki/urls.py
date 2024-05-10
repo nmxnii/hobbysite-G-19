386b88c94f8cb225d234977600944cb1480b9589
Kyleisha Umbay <marie.umbay@student.ateneo.edu>
2024-05-10 16:21:50 +0800

    retrieved deleted edits

--- wiki/urls.py
+++ wiki/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('articles/', WikiArticleView.as_view(), name='wiki_article_list'),
     path('article/<int:pk>', WikiDetailView.as_view(), name='wiki_article_detail'),
     path('article/add', WikiCreateView.as_view(), name='wiki_article_create'),
-    path('article/<int:pk>/edit', WikiUpdateView.as_view(),
-         name='wiki_article_update')
+    path('article/<int:pk>/edit', WikiUpdateView.as_view(), name='wiki_article_update'),
+    path('gallery/articles', ImageGalleryView.as_view(), name='wiki_gallery_view'),
 ]
 
 app_name = 'wiki'
